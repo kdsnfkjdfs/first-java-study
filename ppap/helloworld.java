0b1d77775278b0d7f5375c817b04c3691974d6ce
lee <71934000+kdsnfkjdfs@users.noreply.github.com>
2022-07-16 21:59:47 +0900

    Add files via upload
    첫 자바 공부

---
+++ ppap/helloworld.java
@@ -0,0 +1,15 @@
+package ppap;
+
+public class helloworld {
+	static int minus(int a,int b) {
+		return a-b;
+	}
+	
+	public static void main(String[] args) {
+		int a =1;
+		int result = minus(a,5);
+		
+		System.out.println(result);
+	}
+
+}	
